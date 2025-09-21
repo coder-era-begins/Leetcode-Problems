ed15aea06b216c2e0d13bc8abbed40edb4cec722
coder-era-begins <leetcvishnu@gmail.com>
2025-09-22 00:08:56 +0530

    Update GenerateParantheses.java

--- Back tracking/GenerateParantheses.java
+++ Back tracking/GenerateParantheses.java
@@ -1,1 +1,21 @@
-
+class Solution {
+    static ArrayList<String> list;
+    static void recursion(String result,int openCnt,int closeCnt,int n){
+        if(openCnt+closeCnt==2*n){
+            list.add(result);
+            return;
+        }
+        if(openCnt<n){
+            recursion(result+"(",openCnt+1,closeCnt,n);
+        }
+        if(closeCnt<openCnt){
+            recursion(result+")",openCnt,closeCnt+1,n);
+        }
+    }
+    public ArrayList<String> generateParentheses(int n) {
+        // code here
+        list=new ArrayList<>();
+        recursion("",0,0,n/2);
+        return list;
+    }
+}
